02a091b7c98fed68062c74ebaa5a8ddcc2fd5027
19930516 <qwers134679@naver.com>
2020-06-01 15:25:41 +0900

    helloName()

--- Hello.java
+++ Hello.java
@@ -3,11 +3,14 @@ package gittest;
 public class Hello {
 
 	public static void main(String[] args) {
-		hello();
+		hello("Kakao");
+		hello("Elsa");
+		
 	}
 	
-	public static void hello() {
-		System.out.println("Hello~");
+	public static void hello(String name) {
+		System.out.println("Hello" + name);
 	}
 
+	
 }
